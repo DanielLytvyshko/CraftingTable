0d83db4e4bfce4dcb2cd19a82524ad098d32c213
Daniel <lytvyshkodaniel@gmail.com>
2024-11-25 20:36:04 -0800

    Added more styling

--- crafting-table/src/App.tsx
+++ crafting-table/src/App.tsx
@@ -20,14 +20,21 @@ const App = () => {
     setResult(recipeResult ? recipeResult.result : null);
   };
 
+  const resetGrid = () => {
+    setGrid(Array(3).fill(Array(3).fill(null)));
+  }
+
   return (
     <DndProvider backend={HTML5Backend}> 
       <div className="app">
-        <h1 className="title">Minecraft Crafting Table</h1>
-        <div className="crafting-container">
-          <Items updateGrid={updateGrid} />
-          <CraftingGrid grid={grid} updateGrid={updateGrid} />
-          <ResultSlot result={result} />
+        <div className="CraftingBox">
+          <h1 className="title">Minecraft Crafting Table</h1>
+          <button onClick={resetGrid} className="reset-btn">Reset Crafting Grid</button>
+          <div className="crafting-container">
+            <Items updateGrid={updateGrid} />
+            <CraftingGrid grid={grid} updateGrid={updateGrid} />
+            <ResultSlot result={result} />
+          </div>
         </div>
       </div>
     </DndProvider>
